538c95700c36e8e5fd8ece8fff01d4ee00506e64
Pascal Martineau <pascal@lewebsimple.ca>
2017-01-17 22:37:46 -0500

    Move options around (should break existing configurations), clean up index.js

--- index.js
+++ index.js
@@ -4,68 +4,72 @@ const merge = require('webpack-merge');
 module.exports = function (sails) {
 
   // Validate hook configuration
-  if (!sails.config.webpack || !sails.config.webpack.config) {
+  if (!sails.config.webpack || !sails.config.webpack.options) {
     sails.log.warn('sails-hook-webpack2: No webpack options have been defined.');
     sails.log.warn('sails-hook-webpack2: Please configure your config/webpack.js file.');
     return {};
   }
-  const hookOptions = sails.config.webpack;
 
-  // Actual Sails hook
+  // Sails hook specification
   const hook = {
     emitReady: false,
-    configure() {},
-    initialize(next) {
-      next();
-    },
-    afterBuild(err, rawStats) {
-      if (err) {
-        return sails.log.error('sails-hook-webpack2: Build error: \n\n', err);
-      }
+    afterBuild(err, stats) {
+      if (err) return sails.log.error('sails-hook-webpack2: Build error:\n', err);
       // Emit events
       if (!this.emitReady) {
         sails.emit('hook:sails-hook-webpack2:compiler-ready', {});
         this.emitReady = true;
       }
-      sails.emit('hook:sails-hook-webpack2:after-build', rawStats);
+      sails.emit('hook:sails-hook-webpack2:after-build', stats);
       // Display information, errors and warnings
-      const stats = rawStats.toJson();
-      sails.log.info('sails-hook-webpack2: Build complete.')
-      sails.log.silly(`sails-hook-webpack2: ${rawStats.toString({colors: true, chunks: false})}`);
-      if (stats.errors.length > 0) {
-        sails.log.error('sails-hook-webpack2: ', stats.errors);
-      }
+      stats = stats.toJSON();
+      sails.log.info('sails-hook-webpack2: Build complete.');
       if (stats.warnings.length > 0) {
-        sails.log.warn('sails-hook-webpack2: ', stats.warnings);
+        sails.log.warn('sails-hook-webpack2: Build warnings:\n', stats.warnings);
       }
-      return null;
+      if (stats.errors.length > 0) {
+        sails.log.error('sails-hook-webpack2: Build errors:\n', stats.errors);
+      }
     }
   };
 
-  // Webpack configuration
-  var config = hookOptions.config;
-  if (hookOptions[process.env.NODE_ENV] && hookOptions[process.env.NODE_ENV].config) {
-    config = merge(config, hookOptions[process.env.NODE_ENV].config);
+  const environment = process.env.NODE_ENV || 'development';
+  const host = sails.getHost() || 'localhost';
+  const port = sails.config.port || 1337;
+
+  // Webpack options
+  let options = sails.config.webpack.options;
+  // Merge environment-specific options
+  if (sails.config.webpack[environment]) {
+    options = merge(options, sails.config.webpack[environment]);
   }
-  config = merge(config, {
+  // Merge default options
+  options = merge(options, {
     plugins: [
+      // User environment variables
       new webpack.DefinePlugin({
         'process.env': {
-          'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-          'HOST': JSON.stringify(sails.getHost() || 'localhost'),
-          'PORT': JSON.stringify(sails.config.port || 1337)
+          'NODE_ENV': JSON.stringify(environment),
+          'SAILS_HOST': JSON.stringify(host),
+          'SAILS_PORT': JSON.stringify(port)
         }
       })
-    ]
+    ],
+    performance: {
+      hints: false
+    },
+    stats: 'errors-only'
   });
 
   // Create webpack compiler
-  hook.compiler = webpack(config, (err, stats) => {
-    if (err) throw err;
+  hook.compiler = webpack(options, (err, stats) => {
+    if (err) {
+      sails.log.error('sails-hook-webpack2: Configuration error:\n', err);
+      return {};
+    }
     sails.log.info('sails-hook-webpack2: Webpack configured successfully.');
-    sails.log.silly('sails-hook-webpack2: ', stats.toString());
-    if (process.env.NODE_ENV === 'production') {
-      sails.log.info('sails-hook-webpack2: Running production build...');
+    if (environment === 'production') {
+      sails.log.info('sails-hook-webpack2: Building for production...');
       hook.compiler.run(hook.afterBuild.bind(hook));
     }
     else {
@@ -75,19 +79,20 @@ module.exports = function (sails) {
   });
 
   // Start webpack-dev-server
-  if (process.env.NODE_ENV !== 'production') {
+  if (environment !== 'production' && sails.config.webpack.server) {
     const WebpackDevServer = require('webpack-dev-server');
-    var serverConfig = {
+    // Webpack-dev-server configuration
+    let config = {
       hot: true,
       port: 3000
     };
-    Object.assign(serverConfig, hookOptions.development.server || {});
+    Object.assign(config, sails.config.webpack.server);
 
-    // Listen on specified port
-    hook.server = new WebpackDevServer(hook.compiler, serverConfig);
-    hook.server.listen(serverConfig.port);
-    sails.log.info('sails-hook-webpack2: Server listening on http://' + (sails.getHost() || 'localhost') + ':' + serverConfig.port);
+    // Listen on specific port
+    hook.server = new WebpackDevServer(hook.compiler, config);
+    hook.server.listen(config.port);
+    sails.log.info('sails-hook-webpack2: Server listening on http://' + host + ':' + config.port);
   }
 
   return hook;
-}
+};
